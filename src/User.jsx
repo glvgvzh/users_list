1adfbdcca68024df5a667c15fc5a4c14009a63a2
arina.silenko <291114883+glvgvzh@users.noreply.github.com>
2026-06-21 16:30:05 +0300

    add user actions dropdown menu

--- src/User.jsx
+++ src/User.jsx
@@ -1,12 +1,45 @@
 import {
-    CircleUserRound, MapPin, Mail, Phone, Globe, AtSign, BriefcaseBusiness
+  CircleUserRound, MapPin, Mail, Phone, Globe, AtSign, BriefcaseBusiness, EllipsisVertical,
+  SquarePen, Trash2
 } from 'lucide-react'
+import { useState, useEffect } from 'react'
 
 function User({ user }) {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
 
     <div className="user-card">
+
+      <div className='card-options'>
+        <button className='button-options'
+          onClick={() => setIsMenuOpen(prev => !prev)}
+        >
+          <EllipsisVertical />
+        </button>
+        {isMenuOpen &&
+          <div className='dropdown-menu'>
+            <button className='edit'
+              onClick={() => {
+                setIsMenuOpen(false)
+              }}
+            >
+              <SquarePen />
+              Редактировать
+            </button>
+            <button className='delete'
+              onClick={() => {
+                setIsMenuOpen(false)
+              }}
+            >
+              <Trash2 />
+              Удалить
+            </button>
+          </div>
+        }
+      </div>
+
       <div className="circle-user-pic"><CircleUserRound strokeWidth={0.3} /></div>
       <h3>{user.name}</h3>
 
